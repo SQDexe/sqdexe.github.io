0486063cc6c391d75f386f5f15024ac67f7eb0ec
Jakub Szuchniewicz <66796000+SQDexe@users.noreply.github.com>
2023-07-17 18:33:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 //$.getJSON("https://api.github.com/users/SQDexe/repos", null, (data, textStatus, jqXHR) => {console.log(data)}).fail((jqxhr,settings,ex)=> {console.log("fail")});
-const vars = {
-    display: $(".dots"),
-    counter: 1
-    }
-setInterval(() => {
-    if (3 < vars.counter) vars.counter = 0;
-    let dots = '';
-    for (let i = 0; i < vars.counter; i++) dots += '.';
-    vars.display.html( dots.length == 0 ? "<br>" : dots);
-    vars.counter++;
-    }, 1500)
