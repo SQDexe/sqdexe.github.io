3aa1464acaf7e0cacad62bfc2e863dc125d98180
Jakub Szuchniewicz <66796000+SQDexe@users.noreply.github.com>
2023-07-30 00:35:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //$.getJSON("https://api.github.com/users/SQDexe/repos", null, (data, textStatus, jqXHR) => {console.log(data)}).fail((jqxhr,settings,ex)=> {console.log("fail")});
+lang = [
+    "en",
+    "pl"
+    ]
