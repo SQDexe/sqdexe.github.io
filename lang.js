34878c68b60c88865366947399d7f710b6e71580
Jakub Szuchniewicz <66796000+SQDexe@users.noreply.github.com>
2023-08-03 16:40:23 +0200

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,42 @@
+const lang = Object.freeze([
+    {
+        "name": "Danish",
+        "code": "da",
+        "nativeName": "Dansk"
+    },
+    {
+        "name": "German",
+        "code": "de",
+        "nativeName": "Deutsch"
+    },
+    {
+        "name": "English",
+        "code": "en",
+        "nativeName": "English"
+    },
+    {
+        "name": "Spanish",
+        "code": "es",
+        "nativeName": "Español"
+    },
+    {
+        "name": "French",
+        "code": "fr",
+        "nativeName": "Français"
+    },
+    {
+        "name": "Japanese",
+        "code": "jp",
+        "nativeName": "日本語"
+    },
+    {
+        "name": "Polish",
+        "code": "pl",
+        "nativeName": "Polski"
+    },
+    {
+        "name": "Russian",
+        "code": "ru",
+        "nativeName": "Русский"
+    }
+]);
--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 //$.getJSON("https://api.github.com/users/SQDexe/repos", null, (data, textStatus, jqXHR) => {console.log(data)}).fail((jqxhr,settings,ex)=> {console.log("fail")});
-lang = [
-    "en",
-    "pl"
-    ]
+const vars = {};
 
-const load = () => {
-    for (let ln of lang) {
+const loadButtons = () => {
+    /* Iters over list of languages, and prepares buttons */
+    for (let l of lang) 
         $(".lang-menu")
             .append($("<button></button>")
-                .text(ln.toUpperCase())
+                .text(l.code.toUpperCase())
                 .addClass("button")
                 .addClass("lang-select")
-                .attr("data-lang-value", ln));
-        }
+                .attr("data-value-lang", l.code));
+    }
+const setLang = lang => {
+    console.log(lang);
+    }
+const load = () => {
+    /* Exxecutes preparatory functions */       
+    loadButtons();
+    setLang(lang[3].code);
+    
+    /* Binds events */
+    $(".lang-menu").click(e => {
+        e.target.parent().children().removeAttr("disabled");
+        e.target.attr("disabled", '');
+        setLang(e.target.attr("data-value-lang"));
+        });
     }
 $(document).ready(load);
