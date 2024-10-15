3b1704c44d33426d2a8d58c8438f8f2f48c67f83
Jakub Szuchniewicz <66796000+SQDexe@users.noreply.github.com>
2024-10-15 21:05:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-//$.getJSON("https://api.github.com/users/SQDexe/repos", null, (data, textStatus, jqXHR) => {console.log(data)}).fail((jqxhr,settings,ex)=> {console.log("fail")});
+'use strict';
+//$.getJSON('https://api.github.com/users/SQDexe/repos', null, (data, textStatus, jqXHR) => {console.log(data)}).fail((jqxhr,settings,ex)=> {console.log('fail')});
 const vars = {};
 
 const loadButtons = () => {
     /* Iters over list of languages, and prepares buttons */
     for (let l of lang) 
-        $(".lang-menu")
-            .append($("<button></button>")
+        $('.lang-menu')
+            .append($('<button>')
                 .text(l.code.toUpperCase())
-                .addClass("button")
-                .addClass("lang-select")
-                .attr("data-value-lang", l.code));
+                .addClass('button')
+                .addClass('lang-select')
+                .attr('data-lang-value', l.code));
     }
-const setLang = lang => {
-    console.log(lang);
+const setLang = l => {
+    let selected = lang.find(e => e.code.toLowerCase() == l.toLowerCase());
+    $('[data-lang-translate]').each((_, e) => e.text(selected[$(e).attr('data-lang-translate')]));
     }
 const load = () => {
-    /* Exxecutes preparatory functions */       
+    /* Executes preparatory functions */       
     loadButtons();
-    setLang(lang[3].code);
+    setLang(lang[2].code);
     
     /* Binds events */
-    $(".lang-menu").click(e => {
-        e.target.parent().children().removeAttr("disabled");
-        e.target.attr("disabled", '');
-        setLang(e.target.attr("data-value-lang"));
+    $('.lang-menu').click(e => {
+        e.target.parent().children().removeAttr('disabled');
+        e.target.attr('disabled', '');
+        setLang(e.target.attr('data-lang-value'));
         });
     }
 $(document).ready(load);
